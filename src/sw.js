23b1726dd5523a50bb3c1b7bf1acfa5e72069bb3
Rick Brunstedt <rbrunstedt@gmail.com>
2019-10-23 20:47:20 +0200

    share target, WIP

---
+++ src/sw.js
@@ -0,0 +1,49 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// importScripts(
+//   "precache-manifest.57df311b53bb1486b67d9f2b1bd28c64.js"
+// );
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+/**
+ * Everything below is custom made
+ */
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'POST') {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    (async () => {
+      const formData = await event.request.formData();
+      const link = formData.get('link') || '';
+      const responseUrl = await saveBookmark(link);
+      return Response.redirect(responseUrl, 303);
+    })()
+  );
+});
